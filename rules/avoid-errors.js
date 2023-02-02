1f93787c81dab49491fdd84434d79ef792ed309a
P-Chan <hello@0x50.io>
2023-02-03 07:14:31 +0900

    refactor: re-sort avoid errors

--- rules/avoid-errors.js
+++ rules/avoid-errors.js
@@ -1,46 +1,13 @@
 /**
  * Avoid errors
  *
- * @see https://stylelint.io/user-guide/rules/list/#avoid-errors
+ * @see https://stylelint.io/user-guide/rules/#avoid-errors
  */
 const avoidErrors = {
-  // Annotation
-  'annotation-no-unknown': true,
+  // Descending
+  'no-descending-specificity': true,
 
-  // Color
-  'color-no-invalid-hex': true,
-
-  // Font family
-  'font-family-no-duplicate-names': true,
-  'font-family-no-missing-generic-family-keyword': true,
-
-  // Named grid areas
-  'named-grid-areas-no-invalid': true,
-
-  // Function
-  'function-calc-no-unspaced-operator': true,
-  'function-linear-gradient-no-nonstandard-direction': true,
-  'function-no-unknown': true,
-
-  // String
-  'string-no-newline': true,
-
-  // Unit
-  'unit-no-unknown': true,
-
-  // Custom property
-  'custom-property-no-missing-var-function': true,
-
-  // Property
-  'property-no-unknown': true,
-
-  // Keyframe declaration
-  'keyframe-declaration-no-important': true,
-
-  // Keyframe block
-  'keyframe-block-no-duplicate-selectors': true,
-
-  // Declaration block
+  // Duplicate
   'declaration-block-no-duplicate-custom-properties': true,
   'declaration-block-no-duplicate-properties': [
     true,
@@ -49,32 +16,45 @@ const avoidErrors = {
       ignore: ['consecutive-duplicates'],
     },
   ],
+  'font-family-no-duplicate-names': true,
+  'keyframe-block-no-duplicate-selectors': true,
+  'no-duplicate-at-import-rules': true,
+  'no-duplicate-selectors': true,
+
+  // Empty
+  'block-no-empty': true,
+  'comment-no-empty': true,
+  'no-empty-source': true,
+
+  // Invalid
+  'color-no-invalid-hex': true,
+  'function-calc-no-unspaced-operator': true,
+  'keyframe-declaration-no-important': true,
+  'named-grid-areas-no-invalid': true,
+  'no-invalid-double-slash-comments': true,
+  'no-invalid-position-at-import-rule': true,
+  'string-no-newline': true,
+
+  // Missing
+  'custom-property-no-missing-var-function': true,
+  'font-family-no-missing-generic-family-keyword': true,
+
+  // Non-standard
+  'function-linear-gradient-no-nonstandard-direction': true,
+
+  // Overrides
   'declaration-block-no-shorthand-property-overrides': true,
 
-  // Block
-  'block-no-empty': true,
-
-  // Selector
+  // Unknown
+  'annotation-no-unknown': true,
+  'at-rule-no-unknown': true,
+  'function-no-unknown': true,
+  'media-feature-name-no-unknown': true,
+  'property-no-unknown': true,
   'selector-pseudo-class-no-unknown': true,
   'selector-pseudo-element-no-unknown': true,
   'selector-type-no-unknown': true,
-
-  // Media feature
-  'media-feature-name-no-unknown': true,
-
-  // At-rule
-  'at-rule-no-unknown': true,
-
-  // Comment
-  'comment-no-empty': true,
-
-  // General / Sheet
-  'no-descending-specificity': true,
-  'no-duplicate-at-import-rules': true,
-  'no-duplicate-selectors': true,
-  'no-empty-source': true,
-  'no-invalid-double-slash-comments': true,
-  'no-invalid-position-at-import-rule': true,
+  'unit-no-unknown': true,
 }
 
 module.exports = avoidErrors
